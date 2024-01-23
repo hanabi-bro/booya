cd51fe4930258aca483e1b40c1408dfb3e066051
hanabi-bro <hanabi.bro@gmail.com>
2024-01-23 13:53:01 +0900

    0.0.1

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="booya",
+    version="0.0.1",
+    author="hanabi-bro",
+    author_email="hanabi.bro@gmail.com",
+    description="test",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hanabi-bro/booya",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License 2.0",
+        "Operating System :: OS Independent",
+    ],
+    # entry_points = {
+    #     'console_scripts': ['forti_dff = booya.forti_diff:main']
+    # },
+    python_requires='>=3.9',
+)
